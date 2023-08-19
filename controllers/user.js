a931ad2625034dfac0c584b4d7c64aed5c82cc12
Artemon Chik <zagrousaspear@gmail.com>
2023-08-19 11:07:55 +0200

    fixed hw-04

--- controllers/user.js
+++ controllers/user.js
@@ -61,7 +61,7 @@ const logout = async (req, res, next) => {
   const { _id } = req.user;
   try {
     await User.findByIdAndUpdate(_id, { token: null });
-    res.status(204);
+    res.status(204).json({ message: "No content" });
   } catch (error) {
     next(error);
   }
